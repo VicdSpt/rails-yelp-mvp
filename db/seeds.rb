8a9e9aa668525cc56a6b652ff887a941974c21d4
victor de spirlet <victordespirlet@gmail.com>
2022-11-16 16:16:17 +0100

    punaise exo de ouf

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+SPECIES = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::Restaurant.name
+  restaurant.address = Faker::Address.full_address
+  restaurant.phone_number = Faker::PhoneNumber.cell_phone
+  restaurant.category = SPECIES.sample
+  restaurant.save
+end
